968ad94913d5f8f91a2feaef2ae3acffdc990692
gabrielkazak <142548161+gabrielkazak@users.noreply.github.com>
2025-06-24 20:36:39 -0300

    Delete trabalho_streamlit directory

--- trabalho_streamlit/pages/02_🎮_BestGames.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-st.set_page_config(
-  page_title="Home",
-  page_icon="Gráficos",
-)
-
-st.title('Best Games')
--- trabalho_streamlit/utils/carrega_dados.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd;
-import streamlit as st
-import requests
-
-@st.cache_data
-def carregar_dados():
-    df_original = pd.read.csv('../dataset/backloggd_games_fixed.csv')
-
-    
